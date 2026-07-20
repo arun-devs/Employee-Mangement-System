20319f4783aa9545b89cf8214bcb7b2daa0d6121
Arun <arundevarajan346@gmail.com>
2026-07-20 08:55:47 +0530

    used nav link

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Sidebar=()=>{
     return(
         <div className="flex justify-items-end">
         <div>
-         <Link to="dashboard" className="block">Dashboard</Link>   
-        <Link to="employees" className="block">Employees</Link>   
+         <NavLink to="dashboard" className={({isActive})=>{return isActive? "text-blue-500 font-bold":"block" }}>Dashboard</NavLink>   
+        <NavLink to="employees" className={({isActive})=>{return isActive? "text-blue-500 font-bold":"block"}}>Employees</NavLink>   
         <h2>Departments</h2>
         <h2>Reports</h2>
         <h2>Settings</h2>
