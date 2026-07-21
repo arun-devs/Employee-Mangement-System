08d670028a78bf248ba71ac803e53212660d3e1e
Arun <arundevarajan346@gmail.com>
2026-07-21 08:52:43 +0530

    sidebar ui styles

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
 const Sidebar=()=>{
     return(
-        <div className="flex justify-items-end">
+        <div className="flex justify-items-end w-64 bg-gray-900 text-white min-h-screen p-5">
         <div>
-         <NavLink to="dashboard" className={({isActive})=>{return isActive? "text-blue-500 font-bold":"block" }}>Dashboard</NavLink>   
-        <NavLink to="employees" className={({isActive})=>{return isActive? "text-blue-500 font-bold":"block"}}>Employees</NavLink>   
+         <NavLink to="dashboard" className={({isActive})=>{return isActive? "block px-3 py-2 rounded-md bg-blue-600 font-semibold":"block px-3 py-2 rounded-md hover:bg-gray-700" }}>Dashboard</NavLink>   
+        <NavLink to="employees" className={({isActive})=>{return isActive? "block px-3 py-2 rounded-md bg-blue-600 font-semibold":"block px-3 py-2 rounded-md hover:bg-gray-700"}}>Employees</NavLink>   
         <h2>Departments</h2>
         <h2>Reports</h2>
         <h2>Settings</h2>
